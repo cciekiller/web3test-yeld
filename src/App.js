b1dccfee61922d10a8295976a69cccf08e295e98
cciekiller <74675683+cciekiller@users.noreply.github.com>
2021-01-17 12:10:42 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,200 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { unstable_createMuiStrictModeTheme as createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { Box } from '@material-ui/core';
+import interestTheme from './theme';
+import { colors } from './theme'
+import {
+  Switch,
+  Route
+} from "react-router-dom";
+import IpfsRouter from 'ipfs-react-router'
+import Header from './components/header';
+import StakeSimple from './components/stakeSimple';
+import InvestSimple from './components/investSimple';
+
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+
+const store = require('./stores/store').default.store
+
+interface IAppState {
+  connected: boolean;
+  address: string;
+  chainId: number;
+  networkName: string;
+}
+
+const INITIAL_STATE: IAppState = {
+  connected: false,
+  address: null,
+  chainId: 1,
+  networkName: 'mainnet',
+};
+
+class App extends Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      ...INITIAL_STATE
+    }
+
+    this.web3Modal = new Web3Modal({
+      network: this.state.networkName,
+      cacheProvider: true,
+      providerOptions: this.getProviderOptions()
+    })
+
+    this.ethersProvider = null
+    this.eventProvider = null
+    this.web3Provider = null
+  }
+
+  componentDidMount = async () => {
+    await this.setState({mounted: true})
+    if (this.web3Modal.cachedProvider) {
+      this.setup()
+    }
+  }
+
+  subscribeProvider = async (provider: any) => {
+    if (!provider.on) {
+      return
+    }
+
+    provider.on("close", () => {
+      this.resetApp()
+    })
+
+    provider.on("disconnect", () => {
+      this.resetApp()
+    })
+
+    provider.on("accountsChanged", async (accounts: string[]) => {
+      if (accounts[0] !== this.state.address)
+      {
+        store.setProvider(this.ethersProvider, this.eventProvider, this.state.chainId, accounts[0])
+        await this.setState({ address: accounts[0] });
+      }
+    })
+
+    provider.on("chainChanged", async (chainId: number) => {
+      if (chainId !== this.state.chainId)
+      {
+        store.setProvider()
+        await this.setup()
+      }
+    })
+
+    provider.on("networkChanged", async (networkId: number) => {
+      const network = await this.ethersProvider.getNetwork();
+      const chainId = network.chainId;
+      const networkName = network.name;
+      store.setProvider(this.ethersProvider, this.eventProvider, chainId, this.state.address)
+      await this.setState({ chainId, networkName });
+    })
+  };
+
+  getProviderOptions = () => {
+    const providerOptions = {
+      walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+          infuraId: process.env.REACT_APP_INFURA_ID
+        }
+      }
+    }
+    return providerOptions
+  }
+
+  setup = async () => {
+    if (!this.state.mounted)
+      return
+
+    try {
+      if (!this.web3Provider) {
+        this.web3Provider = await this.web3Modal.connect()
+        await this.subscribeProvider(this.web3Provider)
+      }
+
+      this.ethersProvider = new ethers.providers.Web3Provider(this.web3Provider)
+      const accounts = await this.ethersProvider.listAccounts()
+      const address = accounts[0]
+      const network = await this.ethersProvider.getNetwork()
+      const chainId = network.chainId
+      const networkName = network.name
+      this.eventProvider = ethers.providers.InfuraProvider.getWebSocketProvider(networkName, process.env.REACT_APP_INFURA_ID);
+
+      await this.setState({
+        connected: true,
+        address: address,
+        chainId,
+        networkName
+      })
+      store.setProvider(this.ethersProvider, this.eventProvider, chainId, address)
+    } catch (e) {
+      console.log(e)
+      await this.resetApp()
+    }
+  }
+
+  resetApp = async () => {
+    if (!this.state.mounted)
+      return
+
+    store.setProvider()
+    if (this.ethersProvider) {
+      localStorage.removeItem('walletconnect');
+      this.ethersProvider = null;
+      this.web3Provider = null;
+    }
+    await this.web3Modal.clearCachedProvider();
+    this.setState({ ...INITIAL_STATE });
+  };
+
+  render() {
+    return (
+      <MuiThemeProvider theme={createMuiTheme(interestTheme)}>
+        <CssBaseline />
+        <IpfsRouter>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh',
+            alignItems: 'center',
+            background: colors.white,
+          }}>
+            <Switch>
+              <Route path="/">
+                <Header
+                  onConnect = {this.setup}
+                  onDisconnect = {this.resetApp}
+                  connected = {this.state.connected}
+                  address = {this.state.address}
+                  networkName = {this.state.networkName}
+                />
+                {/* <Vaults /> */}
+                <StakeSimple
+                  connected = {this.state.connected}
+                />
+                <Box style={{
+                  width: "100%",
+                  border: "1px solid #e1e3e6",
+                  borderTop: "none", marginBottom: "30px"}}>
+                </Box>
+                <InvestSimple
+                  connected = {this.state.connected}
+                />
+              </Route>
+            </Switch>
+          </div>
+        </IpfsRouter>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/investSimple/asset.jsx
@@ -0,0 +1,364 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { withStyles } from '@material-ui/core/styles';
+import {
+  Typography,
+  TextField,
+  Button
+} from '@material-ui/core';
+import { withTranslation } from 'react-i18next';
+
+import {
+  ERROR,
+  POOL_INVEST,
+  POOL_REDEEM,
+} from '../../stores/constants'
+
+import Store from "../../stores";
+const emitter = Store.emitter
+const dispatcher = Store.dispatcher
+const store = Store.store
+
+
+const styles = theme => ({
+  value: {
+    cursor: 'pointer'
+  },
+  actionInput: {
+    padding: '0px 0px 12px 0px',
+    fontSize: '0.5rem'
+  },
+  balances: {
+    width: '100%',
+    textAlign: 'right',
+    paddingRight: '20px',
+    cursor: 'pointer'
+  },
+  actionsContainer: {
+    paddingBottom: '12px',
+    display: 'flex',
+    flex: '1',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column'
+    }
+  },
+  title: {
+    paddingRight: '24px'
+  },
+  actionButton: {
+    height: '47px'
+  },
+  tradeContainer: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  sepperator: {
+    borderBottom: '1px solid #E1E1E1',
+    margin: '24px',
+    [theme.breakpoints.up('sm')]: {
+      width: '40px',
+      borderBottom: 'none',
+      margin: '0px'
+    }
+  },
+  scaleContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '0px 0px 12px 0px',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  scale: {
+    minWidth: '10px'
+  },
+  buttonText: {
+    fontWeight: '700',
+  },
+  headingContainer: {
+    width: '100%',
+    display: 'flex',
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    }
+  },
+  heading: {
+    paddingBottom: '12px',
+    flex: 1,
+    flexShrink: 0,
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    }
+  },
+  right: {
+    textAlign: 'right'
+  }
+});
+
+
+class Asset extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      amount: '',
+      amountError: false,
+      redeemAmount: '',
+      redeemAmountError: false,
+      generatedYELD: 0,
+    }
+  }
+
+  componentWillMount() {
+    emitter.on(POOL_INVEST, this.investReturned);
+    emitter.on(POOL_REDEEM, this.redeemReturned);
+    emitter.on(ERROR, this.errorReturned);
+  }
+
+  componentWillUnmount() {
+    emitter.removeListener(POOL_INVEST, this.investReturned);
+    emitter.removeListener(POOL_REDEEM, this.redeemReturned);
+    emitter.removeListener(ERROR, this.errorReturned);
+  };
+
+  investReturned = () => {
+    this.setState({ loading: false, amount: '' })
+  };
+
+  redeemReturned = (txHash) => {
+    this.setState({ loading: false, redeemAmount: '' })
+  };
+
+  errorReturned = (error) => {
+    this.setState({ loading: false })
+  };
+
+  render() {
+    const { classes, asset, t, connected } = this.props;
+    const {
+      amount,
+      amountError,
+      redeemAmount,
+      redeemAmountError,
+      loading
+    } = this.state
+
+    return (<div className={ classes.actionsContainer }>
+      <div className={ classes.tradeContainer }>
+        {!asset.disabled && <div className={ classes.balances }>
+            <Typography variant='h3' className={ classes.title }></Typography><Typography variant='h4' onClick={ () => { this.setAmount(100) } } className={ classes.value } noWrap>{ 'Balance: '+ (asset.balance ? asset.balance.toFixed(4) : '0.0000') } { asset.tokenSymbol ? asset.tokenSymbol : asset.symbol }</Typography>
+        </div>}
+        <TextField
+          fullWidth
+          className={ classes.actionInput }
+          id={'amount'}
+          value={ amount }
+          error={ amountError }
+          onChange={ this.onChange }
+          disabled={ loading || asset.disabled }
+          placeholder="0.00"
+          variant="outlined"
+          onKeyDown={ this.inputKeyDown }
+        />
+        <div className={ classes.scaleContainer }>
+          <Button
+            className={ classes.scale }
+            variant='text'
+            disabled={ loading || asset.disabled }
+            color="primary"
+            onClick={ () => { this.setAmount(25) } }>
+            <Typography variant={'h5'}>25%</Typography>
+          </Button>
+          <Button
+            className={ classes.scale }
+            variant='text'
+            disabled={ loading || asset.disabled }
+            color="primary"
+            onClick={ () => { this.setAmount(50) } }>
+            <Typography variant={'h5'}>50%</Typography>
+          </Button>
+          <Button
+            className={ classes.scale }
+            variant='text'
+            disabled={ loading || asset.disabled }
+            color="primary"
+            onClick={ () => { this.setAmount(75) } }>
+            <Typography variant={'h5'}>75%</Typography>
+          </Button>
+          <Button
+            className={ classes.scale }
+            variant='text'
+            disabled={ loading || asset.disabled }
+            color="primary"
+            onClick={ () => { this.setAmount(100) } }>
+            <Typography variant={'h5'}>100%</Typography>
+          </Button>
+        </div>
+        <Button
+          className={ classes.actionButton }
+          variant="outlined"
+          color="primary"
+          disabled={ loading || !connected || asset.disabled || amount <= 0 }
+          onClick={ async () => {
+              this.onInvest()
+          }}
+          fullWidth
+          >
+          <Typography className={ classes.buttonText } variant={ 'h5'} color={asset.disabled?'':'secondary'}>{asset.disabled? t('Asset.Disabled'):t('Asset.Earn')}</Typography>
+        </Button>
+        <div style={{textAlign: 'center'}}>Depositing new tokens will reset your rewards! Claim them first before depositing</div>
+      </div>
+      <div className={ classes.sepperator }></div>
+      <div className={classes.tradeContainer}>
+        {!asset.disabled && <div className={ classes.balances }>
+          <Typography variant='h3' className={ classes.title }></Typography><Typography variant='h4' onClick={ () => { this.setRedeemAmount(100) } }  className={ classes.value } noWrap>{ asset.investedBalance ? asset.investedBalance.toFixed(4) : '0.0000' } { asset.investSymbol } ({ asset.investedBalance ? (parseFloat(asset.investedBalance)*parseFloat(asset.price)).toFixed(4) : '0' } { asset.tokenSymbol ? asset.tokenSymbol : asset.symbol } )</Typography>
+        </div>}
+        <TextField
+          fullWidth
+          className={ classes.actionInput }
+          id={'redeemAmount'}
+          value={ redeemAmount }
+          error={ redeemAmountError }
+          onChange={ this.onChange }
+          disabled={ loading || asset.disabled }
+          placeholder="0.00"
+          variant="outlined"
+          onKeyDown={ this.inputRedeemKeyDown }
+        />
+        <div className={ classes.scaleContainer }>
+          <Button
+            className={ classes.scale }
+            variant='text'
+            disabled={ loading || asset.disabled }
+            color="primary"
+            onClick={ () => { this.setRedeemAmount(25) } }>
+            <Typography variant={'h5'}>25%</Typography>
+          </Button>
+          <Button
+            className={ classes.scale }
+            variant='text'
+            disabled={ loading || asset.disabled}
+            color="primary"
+            onClick={ () => { this.setRedeemAmount(50) } }>
+            <Typography variant={'h5'}>50%</Typography>
+          </Button>
+          <Button
+            className={ classes.scale }
+            variant='text'
+            disabled={ loading || asset.disabled}
+            color="primary"
+            onClick={ () => { this.setRedeemAmount(75) } }>
+            <Typography variant={'h5'}>75%</Typography>
+          </Button>
+          <Button
+            className={ classes.scale }
+            variant='text'
+            disabled={ loading || asset.disabled}
+            color="primary"
+            onClick={ () => { this.setRedeemAmount(100) } }>
+            <Typography variant={'h5'}>100%</Typography>
+          </Button>
+        </div>
+        <Button
+          className={ classes.actionButton }
+          variant="outlined"
+          color="primary"
+          disabled={ loading || !connected || redeemAmount <= 0 || asset.disabled}
+          onClick={ async () => {
+              this.onRedeem()
+          }}
+          fullWidth
+          >
+          <Typography className={ classes.buttonText } variant={ 'h5'} color={asset.disabled?'':'secondary'}>{ asset.disabled? t('Asset.Disabled'):t('Asset.Claim') }</Typography>
+        </Button>
+        {asset.yeldEarned.toFixed(4)} YELD earned
+      </div>
+    </div>)
+  };
+
+  onChange = (event) => {
+    let val = []
+    val[event.target.id] = event.target.value
+    this.setState(val)
+  }
+
+  inputKeyDown = (event) => {
+    if (event.which === 13) {
+      this.onInvest();
+      return true;
+    }
+    return false;
+  }
+
+  onInvest = () => {
+    this.setState({ amountError: false })
+
+    var { amount } = this.state
+    const { asset, startLoading } = this.props
+
+    if (amount > asset.balance && amount - asset.balance < 1E-4)
+      amount = asset.balance;
+
+    if(!amount || isNaN(amount) || amount <= 0 || amount > asset.balance) {
+      this.setState({ amountError: true })
+      return false
+    }
+
+    this.setState({ loading: true })
+    startLoading()
+    dispatcher.dispatch({ type: POOL_INVEST, content: { amount: amount, asset: asset } })
+  }
+
+  onRedeem = () => {
+    this.setState({ redeemAmountError: false })
+
+    var { redeemAmount } = this.state
+    const { asset, startLoading  } = this.props
+
+    if (redeemAmount > asset.investedBalance && redeemAmount - asset.investedBalance < 1E-4)
+      redeemAmount = asset.investedBalance;
+
+    if(!redeemAmount || isNaN(redeemAmount) || redeemAmount <= 0 || redeemAmount > asset.investedBalance) {
+      this.setState({ redeemAmountError: true })
+      return false
+    }
+
+    this.setState({ loading: true })
+    startLoading()
+
+    dispatcher.dispatch({ type: POOL_REDEEM, content: { amount: redeemAmount, asset: asset } })
+  }
+
+  setAmount = (percent) => {
+
+    if(this.state.loading) {
+      return
+    }
+
+    const { asset } = this.props
+
+    const balance = asset.balance
+    let amount = balance*percent/100
+
+    if(percent === 100 && asset.symbol === 'ETH') {
+        amount = amount - 0.009
+    }
+    this.setState({ amount: amount.toFixed(4) })
+  }
+
+  setRedeemAmount = (percent) => {
+
+    if(this.state.loading) {
+      return
+    }
+
+    const balance = this.props.asset.investedBalance
+    let amount = (balance*percent)/100
+
+    this.setState({ redeemAmount: amount.toFixed(4) })
+  }
+}
+
+export default withTranslation()(withRouter(withStyles(styles, { withTheme: true })(Asset)));
